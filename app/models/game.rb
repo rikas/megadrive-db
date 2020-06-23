ab479f98e5dd911e700f193f8d6ad928f53cc5ef
rikas <oterosantos@gmail.com>
2020-06-23 21:30:30 +0100

    Sort by id to avoid inconsistencies

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,7 +3,7 @@ class Game < ApplicationRecord
   enum cartridge: %i[no_cart cart_md_eu cart_md_jp cart_genesis]
   enum manual: %i[no_manual manual_md_eu manual_md_jp manual_genesis]
 
-  scope :sorted, -> { order("UPPER(name) ASC") }
+  scope :sorted, -> { order(id: :asc) }
   scope :owned, -> { where(owned: true) }
   scope :not_owned, -> { where(owned: false) }
 
