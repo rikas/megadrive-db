ac762af7a47058c8ef9608fa8697d74c3d873c4c
rikas <oterosantos@gmail.com>
2020-06-23 21:26:09 +0100

    Remove testing sleep command

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,7 @@ class GamesController < ApplicationController
   def index
     @games = Game.sorted
 
-    sleep 2
+    # sleep 2 # uncomment to tweak loaders
 
     render json: @games.as_json(methods: :status)
   end
