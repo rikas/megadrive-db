d6c33b2b5d27759acc47d1405b222ef498e99960
rikas <oterosantos@gmail.com>
2021-05-14 15:11:48 +0100

    Add more tests

--- cypress/integration/games_list.spec.js
+++ cypress/integration/games_list.spec.js
@@ -1,5 +1,15 @@
 describe('Games list', () => {
-  it('Successfully loads', () => {
-    cy.visit('/');
+  beforeEach(() => {
+    cy.intercept('GET', '**/games.json', { fixture: 'games.json' })
+    cy.visit('/')
+  });
+
+  it('has a title in the page', () => {
+    cy.get('div.main-title').should('contain', 'My Mega Drive collection');
+  });
+
+  it('returns one game per row', () => {
+    cy.get('table tbody')
+      .find('tr').should('have.length', 20);
   });
 });
